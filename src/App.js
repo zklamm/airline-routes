0f984a2da5cd281c4b02012e91e054ac595e4256
Zac Klammer <zklamm@gmail.com>
2020-09-24 10:30:20 -0500

    Display raw data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import DATA from "./data";
 
 class App extends Component {
   render() {
+    const rows = DATA.routes.map((route, idx) => (
+      <tr key={idx}>
+        <td>{route.airline}</td>
+        <td>{route.src}</td>
+        <td>{route.dest}</td>
+      </tr>
+    ));
+
     return (
       <div className="app">
         <header className="header">
           <h1 className="title">Airline Routes</h1>
         </header>
         <section>
-          <p>
-            Welcome to the app!
-          </p>
+          <table>
+            <thead>
+              <tr>
+                <td>Airline</td>
+                <td>Source Airport</td>
+                <td>Destination Airport</td>
+              </tr>
+            </thead>
+            <tbody>{rows}</tbody>
+          </table>
         </section>
       </div>
     );
